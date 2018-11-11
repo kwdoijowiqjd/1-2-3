f853ac25b9133f7d571921a3c4ac0d5679febec7
kwdoijowiqjd <44804669+kwdoijowiqjd@users.noreply.github.com>
2018-11-11 19:11:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -114,9 +114,9 @@ message.channel.send('#rep <@474573718967025665>')
 
 ////////////////////////////////
 
- bot2.on("ready", async  => {
+ bot2.on("ready", ()  => {
          setInterval(function(){
-        bot2.on('message', message => {
+        bot2.on('message', async message => {
             if(message.content.startsWith('!start')) {
                  const channel2 = bot2.channels.find('510409682121129984')
                     message.channel2.send(`كلام مو مهم`)
